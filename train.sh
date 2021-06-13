fde6fdbeed99e2e99dedbbdca1ee1c283e3dd3ab
Samuel Lo <systemx637@gmail.com>
2021-06-13 19:09:51 +0100

    update script

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --time=00:10:00
+#SBATCH --time=01:00::00
 #SBATCH --gres=gpu:1
 
 nvidia-smi
@@ -10,4 +10,4 @@ source /home/s2122322/anaconda3/bin/activate
 conda activate fs2
 echo Conda environment fs2 activated
 
-python3 train -p config/Herbikher/preprocess.yaml -m config/Herbikher/model.yaml -t config/Herbikher/train.yaml
+python3 train.py -p config/Herbikher/preprocess.yaml -m config/Herbikher/model.yaml -t config/Herbikher/train.yaml
