2264a3ee011649508c05ee5acb0f2733aa4c25ad
Samuel Lo <systemx637@gmail.com>
2021-06-13 17:51:52 +0100

    add train.sh

---
+++ train.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#SBATCH --time=00:30:00
+#SBATCH --gres=gpu:1
+
+nvidia-smi
+
+echo $CUDA_VISIBLE_DEVICES
+source /home/s2122322/anaconda3/bin/activate
+conda activate fs2
+echo Conda environment fs2 activated
+
+python3 train -p config/Herbikher/preprocess.yaml -m config/Herbikher/model.yaml -t config/Herbikher/train.yaml
