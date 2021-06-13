fffd190dbca73a5a37463e38a6e58dc2ce51f624
Samuel Lo <systemx637@gmail.com>
2021-06-13 14:24:59 +0100

    update preprocessor.py

--- preprocessor/preprocessor.py
+++ preprocessor/preprocessor.py
@@ -65,7 +65,7 @@ class Preprocessor:
         # Compute pitch, energy, duration, and mel-spectrogram
         speakers = {}
         for i, speaker in enumerate(tqdm(os.listdir(self.in_dir))):
-            print(speakers)
+            print(f"speaker: {speaker}")
             speakers[speaker] = i
             for wav_name in os.listdir(os.path.join(self.in_dir, speaker)):
                 if ".wav" not in wav_name:
