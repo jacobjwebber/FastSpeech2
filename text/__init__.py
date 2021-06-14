20b10c01e13be312e11fedde517e4d2835af0a29
Samuel Lo <systemx637@gmail.com>
2021-06-15 00:16:57 +0100

    add checkpoint

--- text/__init__.py
+++ text/__init__.py
@@ -38,7 +38,7 @@ def text_to_sequence(text, cleaner_names):
         sequence += _symbols_to_sequence(_clean_text(m.group(1), cleaner_names).split())
         sequence += _symbols_to_sequence(m.group(2))
 
-        #breakpoint()
+        breakpoint()
 
         text = m.group(3)
 
