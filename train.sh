09be3ee67c67fc042aff630648dc0f87fc433781
Samuel Lo <s2122322@uhtred.inf.ed.ac.uk>
2021-06-15 16:12:24 +0100

    edit train.sh and added slurm output

--- train.sh
+++ train.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-#SBATCH --time=01:00:00
+#SBATCH --time=08:00:00
 #SBATCH --gres=gpu:1
 
 nvidia-smi
@@ -8,6 +8,5 @@ nvidia-smi
 echo $CUDA_VISIBLE_DEVICES
 source /home/s2122322/anaconda3/bin/activate
 conda activate fs2
-echo Conda environment fs2 activated
 
 python3 train.py -p config/Herbikher/preprocess.yaml -m config/Herbikher/model.yaml -t config/Herbikher/train.yaml
