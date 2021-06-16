455465c260c19b16417c0a31d865bfe2ce6b5f3f
Samuel Lo <systemx637@gmail.com>
2021-06-16 15:46:37 +0100

    modify symbols

--- text/symbols.py
+++ text/symbols.py
@@ -5,9 +5,9 @@ Defines the set of symbols used in text input to the model.
 
 The default is a set of ASCII characters that works well for English or text that has been run through Unidecode. For other data, you can modify _characters. See TRAINING_DATA.md for details. """
 
-from text import cmudict, pinyin
+#from text import cmudict, pinyin
 
-# [_!()',.:;? -אאַאָבבּבֿגדהווּײװױזחטייִײַכּכךלמםנןסעפּפֿפףצץקרששׂתּת0123456789]
+# [^_!()',.:;? \-אאַאָבבּבֿגדהווּײװױזחטייִײַכּכךלמםנןסעפּפֿפףצץקרששׂתּת0123456789]
 
 _pad = "_"
 _punctuation = "!()',.:;? "
