316da630ea1b3b8ab61542184d0fbced9a67a173
Samuel Lo <systemx637@gmail.com>
2021-06-17 23:23:13 +0100

    add 60k

---
+++ train60k.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#SBATCH --time=08:00:00
+#SBATCH --gres=gpu:2
+
+nvidia-smi
+
+echo $CUDA_VISIBLE_DEVICES
+source /home/s2122322/anaconda3/bin/activate
+conda activate fs2
+
+python3 train.py -p config/Herbikher/preprocess.yaml -m config/Herbikher/model.yaml -t config/Herbikher/train.yaml --restore_step 60000
