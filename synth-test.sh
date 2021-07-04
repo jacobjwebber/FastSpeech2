e334541d482af48729c13cba4f5b8a21cc6f9bc5
Samuel Lo <systemx637@gmail.com>
2021-07-04 17:06:04 +0100

    add scripts

--- synth-test.sh
+++ synth-test.sh
@@ -1,19 +1,9 @@
 #!/bin/sh
 
-#SBATCH --time=01:00:00
-#SBATCH --gres=gpu:1
-
-nvidia-smi
-
-echo $CUDA_VISIBLE_DEVICES
-source /home/s2122322/anaconda3/bin/activate
-conda activate fs2
-
 input="dershlisl_test.txt"
 while IFS= read -r line
 do
   depointed=$(python3 ../yiddish-tts/depoint.py "$line")
-  
   echo "Synthesising with Herbikher..."
   echo "$line"
   python3 synthesize.py --text "$line" --restore_step 75000 --mode single -p config/Herbikher/preprocess.yaml -m config/Herbikher/model.yaml -t config/Herbikher/train.yaml
